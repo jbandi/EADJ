8f0aabc799268fe909354d185a355b8689b14025
jbandi <dev@jonasbandi.net>
2014-01-24 17:06:15 +0100

    Sanitizing EJB lookup names

--- EnterpriseMusicStore/EnterpriseMusicStore-web-jsfunit/src/test/java/org/musicstore/tests/jsfunit/EjbTest.java
+++ EnterpriseMusicStore/EnterpriseMusicStore-web-jsfunit/src/test/java/org/musicstore/tests/jsfunit/EjbTest.java
@@ -1,15 +1,23 @@
 package org.musicstore.tests.jsfunit;
 
+import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import junit.framework.TestCase;
 import org.musicstore.Greeter;
 
+import java.util.Hashtable;
+
 public class EjbTest extends org.apache.cactus.ServletTestCase {
 
      public void test_greetings_message() throws NamingException {
-        InitialContext jndi = new InitialContext();
-        Greeter greeter = (Greeter) jndi.lookup("org.musicstore.Greeter");
+
+         final Hashtable jndiProperties = new Hashtable();
+         jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+
+        InitialContext jndi = new InitialContext(jndiProperties);
+        Greeter greeter = (Greeter) jndi.lookup("ejb:EnterpriseMusicStore/EnterpriseMusicStoreEJB//GreeterService!org.musicstore.Greeter");
+//        Greeter greeter = (Greeter) jndi.lookup("java:app/EnterpriseMusicStoreEJB/GreeterService");
 
         String test = greeter.getMessage();
         
