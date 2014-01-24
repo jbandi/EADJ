5ebcd29917af2d9acb5469fe6befbdf573f041ef
jbandi <dev@jonasbandi.net>
2014-01-24 15:01:45 +0100

    Adding some explanations

--- EnterpriseMusicStore/EnterpriseMusicStore-ejb/src/main/java/org/musicstore/repositories/AlbumQuery.java
+++ EnterpriseMusicStore/EnterpriseMusicStore-ejb/src/main/java/org/musicstore/repositories/AlbumQuery.java
@@ -3,6 +3,7 @@ package org.musicstore.repositories;
 import java.io.Serializable;
 
 public class AlbumQuery implements Serializable {
+    // Query objects are passed from the web to ejb. JBoss requires them to be serializable.
 
     private String title;
     private String genreName;
